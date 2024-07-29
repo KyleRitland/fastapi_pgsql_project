0b2cc22568959211f80b04ad3d031482fa269310
KyleRitland <32915224+KyleRitland@users.noreply.github.com>
2024-07-29 11:30:10 -0400

    Update crud.py

--- backend/app/crud.py
+++ backend/app/crud.py
@@ -1,7 +1,18 @@
 from sqlalchemy.orm import Session
-from sqlalchemy import and_
 
 from . import schemas
+
+from sklearn.metrics.pairwise import cosine_similarity
+
+import torch
+from transformers import BertTokenizer, BertModel
+
+
+'''
+
+testing tweet retrival from table + database
+
+'''
 
 def get_tweets(db: Session, skip: int = 0, limit: int = 100):
     db.execute(
@@ -15,17 +26,9 @@ def get_tweets(db: Session, skip: int = 0, limit: int = 100):
 
 '''
 
-project code
+main project code
 
 '''
-
-
-from collections import Counter
-from nltk.corpus import stopwords
-from sklearn.metrics.pairwise import cosine_similarity
-
-from transformers import BertTokenizer, BertModel
-import torch
 
 def get_tweet_sim(db: Session, text_in: str = "", limit: int = 0):
     print(f'text_in: {text_in}')
@@ -35,40 +38,35 @@ def get_tweet_sim(db: Session, text_in: str = "", limit: int = 0):
     bert_tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
     bert_model = BertModel.from_pretrained('bert-base-uncased')
 
-    example_encoding = bert_tokenizer.batch_encode_plus(
+    #create encoder for text from user
+    text_in_encoding = bert_tokenizer.batch_encode_plus(
         [text_in],
         padding=True,
         truncation=True,
         return_tensors='pt',
         add_special_tokens=True
     )
-    example_input_ids = example_encoding['input_ids']
-    example_attention_mask = example_encoding['attention_mask']
+    text_in_input_ids = text_in_encoding['input_ids']
+    text_in_attention_mask = text_in_encoding['attention_mask']
     
-    # Generate embeddings for the example sentence
+    # Generate embedding
     with torch.no_grad():
-        example_outputs = bert_model(example_input_ids, attention_mask=example_attention_mask)
+        example_outputs = bert_model(text_in_input_ids, attention_mask=text_in_attention_mask)
         example_sentence_embedding = example_outputs.last_hidden_state.mean(dim=1)
 
-    #cos_sim = sorted(cosine_similarity(torch.Tensor([i[4] for i in tweets_all]), example_sentence_embedding), reverse=True)[:limit]
+    #calculate cosine simmilarity between input text embedding and tweet embeddings
+    
     cos_sim = cosine_similarity(torch.Tensor([i[4] for i in tweets_all]), example_sentence_embedding)
-    print(tweets_all[0][3])
-    print(cos_sim[0])
+    
+    #combign tweet text and sim score in one list,
+    # then sort tweets by similarity score to input text and retrieve top N scoring tweets
+
     zip_group = [[tweets_all[i][3], cos_sim[i][0]] for i in range(len(cos_sim))]
     zip_group = sorted(zip_group, key=lambda x: x[1], reverse=True)[:limit]
-    print('zip_group', zip_group)
-    #test_g = zip(tweets_all[:limit], cos_sim)
-    '''for item in test_g:
-        print(item)'''
+    
+    #create list of schema for FastAPI return
     tweets_sim = [schemas.Similarity( text_in=text_in, tweet_text=item[0], sim_score=item[1]) for item in zip_group]
-    print('tweets_keyword',tweets_sim)
-    '''for i in tweets_all[:10]:
-        print()
-        print(type(torch.Tensor(i[4])), type(example_sentence_embedding))
-        
-        print(cos_sim)
-        #print(i[4])
-        print()'''
+    
     return tweets_sim
 
 def get_tweets_top_n(db: Session, top_n_words: int = 5):
@@ -95,16 +93,8 @@ def get_tweets_dr(db: Session, start_dt: str, end_dt: str):
         (start_dt, end_dt,)
         )
     tweets_dr = db.fetchall()
-    #print('............ start test ........')
-    #print(type(tweets_dr[0]))
-    #tweets_dr = [schemas.Tweet(id=item[0], author_id=item[1], created_at=item[2].strftime('%Y-%m-%d'), text_clean=item[3]) for item in tweets_dr]
     tweets_dr = [schemas.Tweet(id=item[0], author_id=item[1], created_at=item[2], text_clean=item[3]) for item in tweets_dr]
         
-    #for i in tweets_dr:
-    #    print(i)
-    #print('............ end test ........')
-    
     return tweets_dr
-    #return db.query(models.Tweet).filter(models.Tweet.created_at >= start_dt).all()
-
+    
 
